c843767f43d864d5a7e1563d51c86ba539fc3093
Rizky Khapidsyah <rizkyhafitsyah@gmail.com>
2020-05-26 14:47:13 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Overload_Method__CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Siswa Siswa1 = new Siswa();
+
+            Siswa1.Cetak("7832468726", "Abdur Rahman");
+            Siswa1.Cetak(70);
+            Siswa1.Cetak(3.33);
+            Console.Read();
+        }
+    }
+}
---
+++ Siswa.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Overload_Method__CS
+{
+    class Siswa
+    {
+        public string NIM;
+        public string Nama;
+        public int Nilai_UTS;
+        public double IPK;
+
+        public void Cetak(String NIM_Siswa, String NamaSiswa)
+        {
+            this.NIM = NIM_Siswa;
+            this.Nama = NamaSiswa;
+            Console.WriteLine("NIM nya Adalah            : {0} ", this.NIM);
+            Console.WriteLine("Namanya Adalah            : {0} ", this.Nama);
+        }
+
+        public void Cetak(int NilaiUTS)
+        {
+            this.Nilai_UTS = Nilai_UTS;
+            Console.WriteLine("Nilai UTS Adalah          : {0} ", this.Nilai_UTS);
+        }
+
+        public void Cetak(double IPK)
+        {
+            this.IPK = IPK;
+            Console.WriteLine("IPK {0} adalah   : {1} ", this.Nama, this.IPK);
+        }
+
+    }
+
+    
+}
